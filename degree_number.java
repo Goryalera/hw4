808b30d3d7a4ec314bb600e158e4497780bde112
Goryalera <yurkova.valeriya@list.ru>
2025-11-07 09:50:04 +0300

    Update degree_number.java

--- degree_number.java
+++ degree_number.java
@@ -1,1 +1,17 @@
+public class Power {
+    public static double power(double x, int n) {
+        if (n == 0) {
+            return 1;
+        } else if (n > 0) {
+            return x * power(x, n - 1);
+        } else {
+            return 1 / power(x, -n);
+        }
+    }
 
+    public static void main(String[] args) {
+        System.out.println(power(2, 3));   // 8.0
+        System.out.println(power(5, 0));   // 1.0
+        System.out.println(power(2, -2));  // 0.25
+    }
+}
