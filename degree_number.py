954e6dd67c872cef4861ed4a8d1a9d62bf7de254
Goryalera <yurkova.valeriya@list.ru>
2025-11-07 09:45:23 +0300

    Update degree_number.py

--- degree_number.py
+++ degree_number.py
@@ -1,1 +1,8 @@
+def power(x, n):
+    if n == 0:
+        return 1
+    elif n > 0:
+        return x * power(x, n - 1)
+    else:  # обработка отрицательной степени
+        return 1 / power(x, -n)
 
